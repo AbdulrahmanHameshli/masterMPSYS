6252c305801946ccb63390ffba9337c3c1d71bc2
Abdulrahman Hameshli <abdulhameshli1@gmail.com>
2024-03-26 10:56:04 +0100

    SSY345 sensor fusion course added

---
+++ SSY345/HA1/Q1_c.m
@@ -0,0 +1,61 @@
+
+
+%Here we give parameters for a Gaussian density. The parameter mu is the mean, and P is the covariance matrix.
+mu = [0; 10];
+P = [0.3 0;0 8];
+
+muz = [5 ;10];
+Pz=[2.3 4;4 8];
+
+%Call your function.
+xy = sigmaEllipse2D(mu, P);
+z = sigmaEllipse2D(muz,Pz);
+%Now plot the generated points. You should see an elongated ellipse stretching from the top left corner to the bottom right. 
+figure(1);
+h1 = plot(xy(1,:), xy(2,:));
+hold on
+h2=plot(z(1,:), z(2,:));
+%Set the scale of x and y axis to be the same. This should be done if the two variables are in the same domain, e.g. both are measured in meters.
+axis equal
+hold on
+%Also plot a star where the mean is, and make it have the same color as the ellipse.
+plot(mu(1), mu(2), '*', 'color', h1.Color);
+plot(muz(1), muz(2), '*', 'color', h2.Color);
+
+
+
+
+function [ xy ] = sigmaEllipse2D( mu, Sigma, level, npoints )
+%SIGMAELLIPSE2D generates x,y-points which lie on the ellipse describing
+% a sigma level in the Gaussian density defined by mean and covariance.
+%
+%Input:
+%   MU          [2 x 1] Mean of the Gaussian density
+%   SIGMA       [2 x 2] Covariance matrix of the Gaussian density
+%   LEVEL       Which sigma level curve to plot. Can take any positive value, 
+%               but common choices are 1, 2 or 3. Default = 3.
+%   NPOINTS     Number of points on the ellipse to generate. Default = 32.
+%
+%Output:
+%   XY          [2 x npoints] matrix. First row holds x-coordinates, second
+%               row holds the y-coordinates. First and last columns should 
+%               be the same point, to create a closed curve.
+
+if nargin < 3
+    level = 3;
+end
+if nargin < 4
+    npoints = 32;
+end
+
+
+[V, D] = eig(Sigma);
+std_devs = sqrt(diag(D));
+angles = linspace(0, 2*pi, npoints - 1);
+circle = [cos(angles); sin(angles)];
+ellipse = level * V * diag(std_devs) * circle;
+ellipse = ellipse + mu;
+ellipse(:,end+1) = ellipse(:,1);
+xy = ellipse;
+end
+
---
+++ SSY345/HA1/Q2_1.m
@@ -0,0 +1,75 @@
+
+f_1 = @(x)[3*x];
+f_2 = @(x)[x.*x.*x];
+[mu_y, Sigma_y, y_s ,x_s]= approxGaussianTransform(0,1,f_2,5000);
+figure(1)
+histogram(y_s);
+mu_y
+Sigma_y
+figure(2)
+histogram(x_s);
+
+
+
+% % True  values:
+% disp('the true values is');
+% [mu_y, Sigma_y] = affineGaussianTransform(0,1, 3, 0)
+
+
+
+
+function [mu_y, Sigma_y, y_s , x_s] = approxGaussianTransform(mu_x, Sigma_x, f, N)
+%approxGaussianTran
+% 
+% sform takes a Gaussian density and a transformation 
+%function and calculates the mean and covariance of the transformed density.
+%
+%Inputs
+%   MU_X        [m x 1] Expected value of x.
+%   SIGMA_X     [m x m] Covariance of x.
+%   F           [Function handle] Function which maps a [m x 1] dimensional
+%               vector into another vector of size [n x 1].
+%   N           Number of samples to draw. Default = 5000.
+%
+%Output
+%   MU_Y        [n x 1] Approximated mean of y.
+%   SIGMA_Y     [n x n] Approximated covariance of y.
+%   ys          [n x N] Samples propagated through f
+
+
+if nargin < 4
+    N = 5000;
+end
+%Your code here
+
+x_s = mvnrnd(mu_x, Sigma_x, N)';
+y_s = f(x_s);
+mu_y = mean(y_s, 2);
+Sigma_y = cov(y_s');
+
+
+end
+
+
+
+function [mu_y, Sigma_y] = affineGaussianTransform(mu_x, Sigma_x, A, b)
+%affineTransformGauss calculates the mean and covariance of y, the 
+%transformed variable, exactly when the function, f, is defined as 
+%y = f(x) = Ax + b, where A is a matrix, b is a vector of the same 
+%dimensions as y, and x is a Gaussian random variable.
+%
+%Input
+%   MU_X        [n x 1] Expected value of x.
+%   SIGMA_X     [n x n] Covariance of x.
+%   A           [m x n] Linear transform matrix.
+%   B           [m x 1] Constant part of the affine transformation.
+%
+%Output
+%   MU_Y        [m x 1] Expected value of y.
+%   SIGMA_Y     [m x m] Covariance of y.
+
+%Your code here
+
+mu_y = A * mu_x + b;
+Sigma_y = A * Sigma_x *A';
+end
