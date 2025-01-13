32d28b541c36dd5eaccd4ba43fa437146e947923
AbdulrahmanHameshli <abdulhameshli1@gmail.com>
2025-01-13 18:29:39 +0100

    EX8 done, a bit scewed in the end but done

--- Germany/Mobile_Robotics/EX8/main.m
+++ Germany/Mobile_Robotics/EX8/main.m
@@ -24,6 +24,8 @@ figure
 hold on 
 scatter(P(1,:),P(2,:),"r","x")
 scatter(P_new_rotated(1,:),P_new_rotated(2,:),"b")
+% mean((P-P_new_rotated).^2,2)
+
 hold off
 
 %%
@@ -39,13 +41,14 @@ for iterations=1:max_iterations
     
     H = zeros(3,3);
     b= zeros(3,1);
-    
+    t = [x(1);x(2)];
+    Theta = x(3);
+
     for i=1:size(P_new,2)
-        t = [x(1);x(2)];
-        Theta = x(3);
         
         
-        h = [cos(Theta) -sin(Theta);sin(Theta) cos(Theta)] * P_new(:,i) + t;
+        
+        h = [cos(Theta) -sin(Theta);sin(Theta) cos(Theta)] *        P_new(:,i) + t;
         e = h - P(:,i);
         j = [eye(2) [-sin(Theta) -cos(Theta); cos(Theta) -sin(Theta)]*P_new(:,i)];
         
@@ -65,13 +68,68 @@ for iterations=1:max_iterations
 end
 
 Theta = x(3);
-P_new_rotated_ICP = [cos(Theta) -sin(Theta);sin(Theta) cos(Theta)] * P_new +[x(1);x(2)];
+P_new_rotated_ICP = [cos(Theta) -sin(Theta);sin(Theta) cos(Theta)] * P_new + [x(1);x(2)];
+
 figure
 hold on
 scatter(P(1,:),P(2,:),"r","x")
 scatter(P_new_rotated_ICP(1,:),P_new_rotated_ICP(2,:),"b")
+% mean((P-P_new_rotated_ICP).^2,2)
+%%
+% C
 
 
+
+clear all
+
+P = load("P.mat").P;
+P_new = load("P_new.mat").P_new;
+  
+x = [0 0 0 0  10 2]';
+Omega = eye(2,2);
+
+
+for i =1 :2
+    H = zeros(6,6);
+    b = zeros(6,1);
+
+    for points=1:size(P_new,2)
+        r1 = x(1:2);
+        r2 = x(3:4);
+        T = x(5:6);
+        h = [r1' ; r2'] * P_new(:,points) + T;
+        j = [P_new(:,points)' 0 0 1 0 ;0 0 P_new(:,points)' 0 1];
+        e = h - P(:,points);
+        
+        H = H + j' * Omega  * j;
+        b = b + j' * Omega * e;
+    
+    end
+    
+    dx = H \ (-b);
+    x = x + dx; 
+end
+A = [x(1:2)';x(3:4)'];
+[U,D,V] = svd(A);
+R = U*V';
+
+
+if det(R) < 0
+    R(:, end) = -R(:, end);
+end
+
+% T = me an(P, 2) - R * mean(P_new, 2);
+T= [x(5)*D(2,2); x(5)*D(1,1) ];
+
+P_new_rotated_ICP_relaxed = R * P_new +  T;
+
+
+
+figure
+hold on
+scatter(P(1,:),P(2,:),"r","x")
+scatter(P_new_rotated_ICP_relaxed(1,:),P_new_rotated_ICP_relaxed(2,:),"b")
+% mean((P-P_new_rotated_ICP_relaxed).^2,2)
 
 
 
